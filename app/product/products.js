c7729c08dcdaa013e68ef929ce8f30d24519667a
andersonzanichelli <anderzanic@gmail.com>
2015-02-15 22:12:09 -0200

    separação da listagem de produtos em um template

--- app/product/products.js
+++ app/product/products.js
@@ -1,6 +1,12 @@
 angular.module('sportStore.products', [
 ])
 .constant('dataUrl', 'http://localhost:9000/api/products')
+.directive('productsList', function(){
+    return {
+        restrict: 'E',
+        templateUrl: 'product/products-list.html'
+    };
+})
 .controller('productsCtrl', function($scope, $http, dataUrl){
     $scope.data = {};
     $http.get(dataUrl)
