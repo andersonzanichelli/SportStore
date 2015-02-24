69a44dd3e6993fa0971dbbe7ca9f6af63cdc4e03
andersonzanichelli <anderzanic@gmail.com>
2015-02-23 23:05:15 -0300

    Adicionado persistencia da compra do cliente

--- app/product/products.js
+++ app/product/products.js
@@ -1,13 +1,14 @@
 angular.module('sportStore.products', [
 ])
 .constant('dataUrl', 'http://localhost:2403/product')
+.constant('orderUrl', 'http://localhost:2403/orders')
 .directive('productsList', function(){
     return {
         restrict: 'E',
         templateUrl: 'product/products-list.html'
     };
 })
-.controller('productsCtrl', function($scope, $http, dataUrl){
+.controller('productsCtrl', function($scope, $http, $location, dataUrl, orderUrl, cart){
     $scope.data = {};
     $http.get(dataUrl)
         .success(function(data){
@@ -15,5 +16,23 @@ angular.module('sportStore.products', [
         })
         .error(function(error){
             $scope.data.error = error;
-        })
+        });
+    
+    $scope.sendOrder = function(shippingDetails){
+        var order = angular.copy(shippingDetails);
+        order.products = cart.getProducts();
+        
+        $http.post(orderUrl, order)
+            .success(function(data){
+                $scope.data.orderId = data.id;
+                cart.getProducts().length = 0;
+            })
+            .error(function(){
+                $scope.data.orderError = error;
+            })
+            .finally(function(){
+                $location.path("/complete");
+            });
+    }
+    
 });
